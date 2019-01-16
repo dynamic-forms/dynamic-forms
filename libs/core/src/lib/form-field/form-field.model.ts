94dc5c6d9494df227af1d2da7ba7bb1d72373989
Alexander Gebuhr <AlexanderGebuhr@googlemail.com>
2019-01-16 21:26:48 +0100

    Improved form field factory.

--- libs/core/src/lib/form-field/form-field.model.ts
+++ libs/core/src/lib/form-field/form-field.model.ts
@@ -1,7 +1,13 @@
+import { Type } from '@angular/core';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 export type FormTemplateType = 'group' | 'array' | 'control';
 export type FormControlType = FormGroup | FormArray | FormControl;
+
+export interface FormFieldConfig {
+  type: FormTemplateType;
+  component: Type<any>;
+}
 
 export interface FormFieldTemplate {
   key: string;
