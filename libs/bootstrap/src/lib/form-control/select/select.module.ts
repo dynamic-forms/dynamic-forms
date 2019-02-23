f8ec84352f1a3dcd0f5f527dd4ced884dedf8248
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-02-23 12:05:48 +0100

    Clean up of bootstrap modules.

--- libs/bootstrap/src/lib/form-control/select/select.module.ts
+++ libs/bootstrap/src/lib/form-control/select/select.module.ts
@@ -1,16 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
 import { SelectComponent } from './select.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatSelectModule
+    ReactiveFormsModule
   ],
   declarations: [
     SelectComponent
--- libs/bootstrap/src/lib/form-control/textbox/textbox.module.ts
+++ libs/bootstrap/src/lib/form-control/textbox/textbox.module.ts
@@ -1,16 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { TextboxComponent } from './textbox.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule
+    ReactiveFormsModule
   ],
   declarations: [
     TextboxComponent
