fbb5cabcb69dbe149c10283ed9ea497dcf40748b
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-02-24 15:14:28 +0100

    Fixed dynamic form input configs.

--- libs/bootstrap/src/lib/dynamic-form-input/dynamic-form-input.config.ts
+++ libs/bootstrap/src/lib/dynamic-form-input/dynamic-form-input.config.ts
@@ -3,6 +3,7 @@ import { CheckboxComponent } from './checkbox/checkbox.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { NumberboxComponent } from './numberbox/numberbox.component';
+import { TextareaComponent } from './textarea/textarea.component';
 import { TextboxComponent } from './textbox/textbox.component';
 
 export const bsDynamicFormInputConfig: DynamicFormInputConfig = {
@@ -11,6 +12,7 @@ export const bsDynamicFormInputConfig: DynamicFormInputConfig = {
     { type: 'datepicker', component: DatepickerComponent, wrappers: [ 'label' ] },
     { type: 'dropdown', component: DropdownComponent, wrappers: [ 'label' ] },
     { type: 'numberbox', component: NumberboxComponent, wrappers: [ 'label' ] },
+    { type: 'textarea', component: TextareaComponent, wrappers: [ 'label' ] },
     { type: 'textbox', component: TextboxComponent, wrappers: [ 'label' ] }
   ]
 };
