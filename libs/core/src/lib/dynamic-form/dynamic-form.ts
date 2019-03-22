4103d0dd5ff43b4a24467e31bb30b04cecb50359
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-03-22 07:08:18 +0100

    Added unit tests for dynamci form

--- libs/core/src/lib/dynamic-form/dynamic-form.ts
+++ libs/core/src/lib/dynamic-form/dynamic-form.ts
@@ -2,7 +2,7 @@ import { DynamicFormGroup } from '../dynamic-form-group/dynamic-form-group';
 import { DynamicFormTemplate } from './dynamic-form-template';
 
 export class DynamicForm extends DynamicFormGroup {
-  constructor(template: DynamicFormTemplate, model: any = null) {
+  constructor(template: DynamicFormTemplate, model: any) {
     super(null, null, template, model);
   }
 }
