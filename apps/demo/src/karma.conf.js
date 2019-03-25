e7cf590e899f4f544b9eaf737ec218d10e1490da
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-03-25 08:39:43 +0100

    Updated output paths for code coverage.

--- apps/demo/src/karma.conf.js
+++ apps/demo/src/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, '../coverage/@dynamic-forms/demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
