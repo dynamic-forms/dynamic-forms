f6177fefac754cc47d8070d293221909f0abff79
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-04-07 20:11:42 +0200

    Refactoring.

--- libs/material/src/lib/dynamic-form-input/dynamic-form-input.config.ts
+++ libs/material/src/lib/dynamic-form-input/dynamic-form-input.config.ts
@@ -1,11 +1,11 @@
 import { DynamicFormInputConfig } from '@dynamic-forms/core';
-import { CheckboxComponent } from './checkbox/checkbox.component';
-import { DatepickerComponent } from './datepicker/datepicker.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
-import { NumberboxComponent } from './numberbox/numberbox.component';
-import { RadioComponent } from './radio/radio.component';
-import { TextareaComponent } from './textarea/textarea.component';
-import { TextboxComponent } from './textbox/textbox.component';
+import { CheckboxComponent } from './dynamic-form-checkbox/checkbox.component';
+import { DatepickerComponent } from './dynamic-form-datepicker/datepicker.component';
+import { DropdownComponent } from './dynamic-form-dropdown/dropdown.component';
+import { NumberboxComponent } from './dynamic-form-numberbox/numberbox.component';
+import { RadioComponent } from './dynamic-form-radio/radio.component';
+import { TextareaComponent } from './dynamic-form-textarea/textarea.component';
+import { TextboxComponent } from './dynamic-form-textbox/textbox.component';
 
 export const matDynamicFormInputConfig: DynamicFormInputConfig = {
   types: [
--- libs/material/src/lib/dynamic-form-input/dynamic-form-input.module.ts
+++ libs/material/src/lib/dynamic-form-input/dynamic-form-input.module.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MAT_LABEL_GLOBAL_OPTIONS } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CheckboxModule } from './checkbox/checkbox.module';
-import { DatepickerModule } from './datepicker/datepicker.module';
-import { DropdownModule } from './dropdown/dropdown.module';
-import { NumberboxModule } from './numberbox/numberbox.module';
-import { RadioModule } from './radio/radio.module';
-import { TextareaModule } from './textarea/textarea.module';
-import { TextboxModule } from './textbox/textbox.module';
+import { CheckboxModule } from './dynamic-form-checkbox/checkbox.module';
+import { DatepickerModule } from './dynamic-form-datepicker/datepicker.module';
+import { DropdownModule } from './dynamic-form-dropdown/dropdown.module';
+import { NumberboxModule } from './dynamic-form-numberbox/numberbox.module';
+import { RadioModule } from './dynamic-form-radio/radio.module';
+import { TextareaModule } from './dynamic-form-textarea/textarea.module';
+import { TextboxModule } from './dynamic-form-textbox/textbox.module';
 
 @NgModule({
   imports: [
