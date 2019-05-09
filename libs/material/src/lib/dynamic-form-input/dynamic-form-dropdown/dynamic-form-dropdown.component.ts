3a58763177cf44a088e7340985a4f2974edb764d
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-05-09 20:39:44 +0200

    Improved validation of dynamic form inputs.

--- libs/material/src/lib/dynamic-form-input/dynamic-form-dropdown/dynamic-form-dropdown.component.ts
+++ libs/material/src/lib/dynamic-form-input/dynamic-form-dropdown/dynamic-form-dropdown.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { DynamicFormDropdown, DynamicFormInputComponent } from '@dynamic-forms/core';
+import { DynamicFormDropdown, DynamicFormValidationService } from '@dynamic-forms/core';
+import { MatDynamicFormInputComponent } from '../mat-dynamic-form-input.component';
 
 @Component({
   selector: 'mat-dynamic-form-dropdown',
   templateUrl: './dynamic-form-dropdown.component.html'
 })
-export class DynamicFormDropdownComponent extends DynamicFormInputComponent<DynamicFormDropdown> {}
+export class DynamicFormDropdownComponent extends MatDynamicFormInputComponent<DynamicFormDropdown> {
+  constructor(protected validationService: DynamicFormValidationService) {
+    super(validationService);
+  }
+}
--- libs/material/src/lib/dynamic-form-input/dynamic-form-numberbox/dynamic-form-numberbox.component.ts
+++ libs/material/src/lib/dynamic-form-input/dynamic-form-numberbox/dynamic-form-numberbox.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { DynamicFormInputComponent, DynamicFormNumberbox } from '@dynamic-forms/core';
+import { DynamicFormNumberbox, DynamicFormValidationService } from '@dynamic-forms/core';
+import { MatDynamicFormInputComponent } from '../mat-dynamic-form-input.component';
 
 @Component({
   selector: 'mat-dynamic-form-numberbox',
   templateUrl: './dynamic-form-numberbox.component.html'
 })
-export class DynamicFormNumberboxComponent extends DynamicFormInputComponent<DynamicFormNumberbox> {}
+export class DynamicFormNumberboxComponent extends MatDynamicFormInputComponent<DynamicFormNumberbox> {
+  constructor(protected validationService: DynamicFormValidationService) {
+    super(validationService);
+  }
+}
