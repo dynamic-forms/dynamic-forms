e3d0ea3c32c9875d9a2d17835991cb420327bca2
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-05-16 08:07:28 +0200

    Fixed lint issue.

--- apps/demo/src/app/layout/navbar/examples-menu/examples-menu.module.ts
+++ apps/demo/src/app/layout/navbar/examples-menu/examples-menu.module.ts
@@ -4,8 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { ExamplesMenuListComponent } from './examples-menu-list.component';
 import { ExamplesMenuComponent } from './examples-menu.component';
