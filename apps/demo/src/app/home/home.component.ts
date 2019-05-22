3a364710283d6c073a81e0b767a941f89f8285ce
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-05-22 08:19:56 +0200

    Improvement of home component.

--- apps/demo/src/app/home/home.component.ts
+++ apps/demo/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {}
