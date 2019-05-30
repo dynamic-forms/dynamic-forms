d63cbef74fa4943978323b4fa1445d963cbd1356
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-05-30 10:10:25 +0200

    Updated loadChildren methods.

--- apps/demo/src/app/app-routing.module.ts
+++ apps/demo/src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ export const appRoutes: Routes = [
   },
   {
     path: 'examples/bootstrap',
-    loadChildren: './examples/bootstrap/bootstrap-examples.module#BootstrapExamplesModule'
+    loadChildren: () => import('./examples/bootstrap/bootstrap-examples.module').then(m => m.BootstrapExamplesModule)
   },
   {
     path: 'examples/material',
-    loadChildren: './examples/material/material-examples.module#MaterialExamplesModule'
+    loadChildren: () => import('./examples/material/material-examples.module').then(m => m.MaterialExamplesModule)
   }
 ];
 
