5eebd66dd05fd49e313f442179e090eaf92a5043
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-06-02 11:53:52 +0200

    Fixed unit test.

--- libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-label-wrapper.component.spec.ts
+++ libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-label-wrapper.component.spec.ts
@@ -1,12 +1,12 @@
 import { async, TestBed } from '@angular/core/testing';
 import { DynamicFormLabelWrapperBootstrapComponent } from './dynamic-form-label-wrapper.component';
-import { DynamicFormWrapperBootstrapModule } from './dynamic-form-wrapper.module';
+import { BsDynamicFormWrapperModule } from './dynamic-form-wrapper.module';
 
 describe('DynamicFormLabelWrapperBootstrapComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        DynamicFormWrapperBootstrapModule
+        BsDynamicFormWrapperModule
       ]
     });
   }));
