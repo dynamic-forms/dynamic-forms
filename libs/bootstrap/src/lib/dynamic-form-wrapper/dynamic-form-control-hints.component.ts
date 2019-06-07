3396066104f640f81ad7e3e85153f57d9772d182
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-06-07 07:34:50 +0200

    Improved component for dynamic form control hints.

--- libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-control-hints.component.ts
+++ libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-control-hints.component.ts
@@ -1,17 +1,15 @@
 import { Component, ViewContainerRef } from '@angular/core';
-import { DynamicFormWrapper } from '@dynamic-forms/core';
+import { DynamicFormControl, DynamicFormWrapper } from '@dynamic-forms/core';
 
 @Component({
   selector: 'bs-dynamic-form-control-hints',
   templateUrl: './dynamic-form-control-hints.component.html',
   styleUrls: ['./dynamic-form-control-hints.component.scss']
 })
-export class BsDynamicFormControlHintsComponent extends DynamicFormWrapper {
+export class BsDynamicFormControlHintsComponent extends DynamicFormWrapper<DynamicFormControl> {
   constructor(protected containerRef: ViewContainerRef) {
     super(containerRef);
   }
 
-  get hints() {
-    return this.template['hints'];
-  }
+  get hints() { return this.field.template.hints; }
 }
