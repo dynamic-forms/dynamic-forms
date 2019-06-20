1e1893a5fa5dc8849a825266a8c35196030c00a8
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-06-21 00:00:44 +0200

    Added expression state to support memoized JS sexpressions.

--- libs/core/src/lib/dynamic-form-expression/dynamic-form-field-expression.ts
+++ libs/core/src/lib/dynamic-form-expression/dynamic-form-field-expression.ts
@@ -1,6 +1,29 @@
 import { DynamicFormField } from './../dynamic-form-field/dynamic-form-field';
-import { DynamicFormExpression } from './dynamic-form-expression';
+import { DynamicFormExpression, DynamicFormExpressionState } from './dynamic-form-expression';
+import { DynamicFormExpressionDependency } from './dynamic-form-expression';
+import { DynamicFormExpressionFunction } from './dynamic-form-expression';
 
-export interface DynamicFormFieldExpression extends DynamicFormExpression {
-  field: DynamicFormField;
+export class DynamicFormFieldExpression implements DynamicFormExpression {
+  static readonly dependencyArgs = [
+    { name: 'model', pattern: /model+[.\w]+/g },
+    { name: 'parentModel', pattern: /parentModel+[.\w]+/g },
+    { name: 'rootModel', pattern: /rootModel+[.\w]+/g }
+  ];
+  static readonly args = DynamicFormFieldExpression.dependencyArgs.map(arg => arg.name).concat([ 'state' ]);
+
+  private _state: DynamicFormExpressionState;
+
+  constructor(
+    readonly deps: DynamicFormExpressionDependency[],
+    readonly func: DynamicFormExpressionFunction,
+    readonly field: DynamicFormField
+  ) {
+    this._state = {};
+  }
+
+  get value() {
+    this._state.previousValue = this._state.currentValue;
+    this._state.currentValue = this.func(this.field.model, this.field.parent.model, this.field.root.model, this._state);
+    return this._state.currentValue;
+  }
 }
