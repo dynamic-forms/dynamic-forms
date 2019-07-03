60d1792b5b2340c947f7104bcd5c9a33fb7b55ab
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-07-03 18:53:40 +0200

    Improved HTML templates of inputs and examples.

--- libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-control-label.component.ts
+++ libs/bootstrap/src/lib/dynamic-form-wrapper/dynamic-form-control-label.component.ts
@@ -1,13 +1,15 @@
 import { Component, ViewContainerRef } from '@angular/core';
-import { DynamicFormWrapper } from '@dynamic-forms/core';
+import { DynamicFormControl, DynamicFormWrapper } from '@dynamic-forms/core';
 
 @Component({
   selector: 'bs-dynamic-form-control-label',
   templateUrl: './dynamic-form-control-label.component.html',
   styleUrls: ['./dynamic-form-control-label.component.scss']
 })
-export class BsDynamicFormControlLabelComponent extends DynamicFormWrapper {
+export class BsDynamicFormControlLabelComponent extends DynamicFormWrapper<DynamicFormControl> {
   constructor(protected containerRef: ViewContainerRef) {
     super(containerRef);
   }
+
+  get validation() { return this.field.template.validation; }
 }
