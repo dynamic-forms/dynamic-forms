8d76edd3a013f073204dd7f745434cb3a6fa5dba
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-08-23 09:38:07 +0200

    Code cleanup.

--- libs/core/src/lib/dynamic-form-field/dynamic-form-field-options.ts
+++ libs/core/src/lib/dynamic-form-field/dynamic-form-field-options.ts
@@ -1,7 +1,3 @@
-export interface DynamicFormFieldUpdateDebounce {
-  duration: number;
-}
-
 export type DynamicFormFieldUpdate = 'change' | 'debounce' | 'blur';
 
 export interface DynamicFormFieldOptions {
