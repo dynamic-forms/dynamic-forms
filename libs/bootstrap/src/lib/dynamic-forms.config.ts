96cc69cb59308abc4d8e63f450301c42b74d34d0
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-11-30 17:29:52 +0100

    Refactoring.

--- libs/bootstrap/src/lib/dynamic-forms.config.ts
+++ libs/bootstrap/src/lib/dynamic-forms.config.ts
@@ -1,6 +1,6 @@
 import { dynamicFormConfig, DynamicFormConfig } from '@dynamic-forms/core';
+import { bsDynamicFormFieldWrapperConfig } from './dynamic-form-field-wrapper/dynamic-form-field-wrapper.config';
 import { bsDynamicFormInputConfig } from './dynamic-form-input/dynamic-form-input.config';
-import { bsDynamicFormFieldWrapperConfig } from './dynamic-form-wrapper/dynamic-form-field-wrapper.config';
 
 export const bsDynamicFormConfig: DynamicFormConfig = {
   library: 'bootstrap',
--- libs/bootstrap/src/lib/dynamic-forms.module.ts
+++ libs/bootstrap/src/lib/dynamic-forms.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { getDynamicFormProviders, DynamicFormsModule, DynamicFormConfig } from '@dynamic-forms/core';
+import { BsDynamicFormFieldWrapperModule } from './dynamic-form-field-wrapper/dynamic-form-field-wrapper.module';
 import { BsDynamicFormInputModule } from './dynamic-form-input/dynamic-form-input.module';
-import { BsDynamicFormFieldWrapperModule } from './dynamic-form-wrapper/dynamic-form-field-wrapper.module';
 import { bsDynamicFormConfig } from './dynamic-forms.config';
 
 @NgModule({
