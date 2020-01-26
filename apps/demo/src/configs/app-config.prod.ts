88ef9ed20d6282fc05d7c178dd28615f905d6e61
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2020-01-26 12:58:21 +0100

    Improvements of app config.

---
+++ apps/demo/src/configs/app-config.prod.ts
@@ -0,0 +1,24 @@
+import { AppConfig } from './../app/app-config';
+
+export const appConfig: AppConfig = {
+  version: '1.0.0-preview',
+  build: '',
+  project: {
+    url: 'https://dev.azure.com/alexandergebuhr/dynamic-forms'
+  },
+  repository: {
+    url: 'https://dev.azure.com/alexandergebuhr/_git/dynamic-forms',
+    branch: 'master',
+    libraryQuery: 'path=%2Flibs%2F{{library}}&version=GB{{branch}}'
+  },
+  versions: [
+    {
+      name: 'Latest',
+      url: 'https://dynamic-forms.azurewebsites.net'
+    },
+    {
+      name: 'Next',
+      url: 'https://dynamic-forms.azurewebsites.net/next'
+    }
+  ]
+};
