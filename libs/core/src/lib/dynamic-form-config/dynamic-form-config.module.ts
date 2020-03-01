8fe12efd7214018b29139ee4839d2f5da5cd5968
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2020-03-01 13:36:59 +0100

    Refactoring.

--- libs/core/src/lib/dynamic-form-config/dynamic-form-config.module.ts
+++ libs/core/src/lib/dynamic-form-config/dynamic-form-config.module.ts
@@ -4,30 +4,15 @@ import { DynamicFormElementType, DYNAMIC_FORM_ELEMENT_TYPES } from '../dynamic-f
 import { DynamicFormFieldType, DYNAMIC_FORM_FIELD_TYPES } from '../dynamic-form-field/dynamic-form-field-type';
 import { DynamicFormFieldWrapperType, DYNAMIC_FORM_FIELD_WRAPPER_TYPES } from '../dynamic-form-field/dynamic-form-field-wrapper-type';
 import { DynamicFormInputType, DYNAMIC_FORM_INPUT_TYPES } from '../dynamic-form-input/dynamic-form-input-type';
-import { DynamicFormLibrary, DYNAMIC_FORM_LIBRARY } from '../dynamic-form-library/dynamic-form-library';
-import { DynamicFormLibraryService } from '../dynamic-form-library/dynamic-form-library.service';
 import { dynamicFormValidationConfig, DynamicFormValidationConfig, DYNAMIC_FORM_VALIDATION_CONFIGS } from '../dynamic-form-validation/dynamic-form-validation-config';
 import { DynamicFormConfigService } from './dynamic-form-config.service';
 
 @NgModule({
   providers: [
-    DynamicFormLibraryService,
     DynamicFormConfigService
   ]
 })
 export class DynamicFormConfigModule {
-  static forLibrary(library: DynamicFormLibrary): ModuleWithProviders<DynamicFormConfigModule> {
-    return {
-      ngModule: DynamicFormConfigModule,
-      providers: [
-        {
-          provide: DYNAMIC_FORM_LIBRARY,
-          useValue: library
-        }
-      ]
-    };
-  }
-
   static withElement(elementType: DynamicFormElementType): ModuleWithProviders<DynamicFormConfigModule> {
     return {
       ngModule: DynamicFormConfigModule,
