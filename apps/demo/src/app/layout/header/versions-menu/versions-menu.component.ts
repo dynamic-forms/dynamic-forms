05a9f3e2f6dfc8c9079543dbc3ba064f3877d3d2
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2020-03-12 07:48:55 +0100

    Fixed lint issue.

--- apps/demo/src/app/layout/header/versions-menu/versions-menu.component.ts
+++ apps/demo/src/app/layout/header/versions-menu/versions-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Config, CONFIG } from '../../../state/config/config.model';
