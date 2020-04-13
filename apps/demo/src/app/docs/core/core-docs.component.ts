53c65501df4d0dc81b900e4015c1b15493309854
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2020-04-13 11:48:24 +0200

    Implementation of doc components.

--- apps/demo/src/app/docs/core/core-docs.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-core-docs',
-  templateUrl: './core-docs.component.html',
-  styleUrls: ['./core-docs.component.scss']
-})
-export class CoreDocsComponent {}
---
+++ apps/demo/src/app/docs/core/doc/core-doc.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-doc',
+  templateUrl: './core-doc.component.html',
+  styleUrls: ['./core-doc.component.scss']
+})
+export class CoreDocComponent {}
