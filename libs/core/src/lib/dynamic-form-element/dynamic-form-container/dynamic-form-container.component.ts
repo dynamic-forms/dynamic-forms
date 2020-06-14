1e9b2678585aa663868843dc937af732150b37d8
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2020-06-14 10:17:39 +0200

    Improvement of generics.

--- libs/core/src/lib/dynamic-form-element/dynamic-form-container/dynamic-form-container.component.ts
+++ libs/core/src/lib/dynamic-form-element/dynamic-form-container/dynamic-form-container.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { DynamicFormElement } from '../dynamic-form-element';
 import { DynamicFormElementBase } from '../dynamic-form-element-base';
+import { DynamicFormContainerDefinition } from './dynamic-form-container-definition';
 import { DynamicFormContainerTemplate } from './dynamic-form-container-template';
 
 @Component({
   selector: 'dynamic-form-container',
   templateUrl: './dynamic-form-container.component.html'
 })
-export class DynamicFormContainerComponent extends DynamicFormElementBase<DynamicFormContainerTemplate> {
+export class DynamicFormContainerComponent<
+  Template extends DynamicFormContainerTemplate = DynamicFormContainerTemplate,
+  Definition extends DynamicFormContainerDefinition<Template> = DynamicFormContainerDefinition<Template>
+> extends DynamicFormElementBase<Template, Definition> {
+
   constructor() {
     super();
   }
