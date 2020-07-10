cbe554406dda692ddc22f3179665dc5d400ffe45
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2020-07-10 17:52:45 +0200

    Basic e2e tests for examples.

--- apps/demo-e2e/src/examples/examples.e2e-spec.ts
+++ apps/demo-e2e/src/examples/examples.e2e-spec.ts
@@ -1,11 +1,24 @@
-import { ExamplesPage } from './examples.po';
+import { ExamplesMenu, ExamplesMenuItem } from 'apps/demo/src/app/layout/header/examples-menu/examples-menu';
+import { Example, ExamplesPage } from './examples.po';
+
+const examplesConfig = require('../../../demo/src/assets/examples-menu.json');
+
+export function getExamples(items: ExamplesMenuItem[], namePrefix?: string): Example[] {
+  return items.reduce((result, item) => {
+    const name = namePrefix ? `${namePrefix} - ${item.label}` : item.label;
+    return item.items && item.items.length
+      ? result.concat(getExamples(item.items, name))
+      : result.concat({ id: item.id, modelId: item.modelId, name });
+  }, []);
+}
 
 describe('dynamic-forms-demo examples', () => {
   const themes = [ 'bootstrap', 'material' ];
+  const examples = getExamples((examplesConfig as ExamplesMenu).items);
 
-  themes.forEach((theme) => {
+  themes.forEach(theme => {
     describe(`for theme ${theme}`, () => {
-      let page: ExamplesPage;
+      let page;
 
       beforeEach(() => {
         page = new ExamplesPage(theme);
@@ -16,6 +29,20 @@ describe('dynamic-forms-demo examples', () => {
 
         expect(page.getUrl()).toContain(`/examples/${theme}`);
         expect(page.getTitle()).toEqual('dynamic-forms');
+      });
+
+      examples.forEach(example => {
+        const description = example.modelId
+          ? `for example "${example.name}" with id "${example.id}" and model id "${ example.modelId }"`
+          : `for example "${example.name}" with id "${example.id}"`;
+
+        describe(description, () => {
+          it('has url and title', () => {
+            page.navigateToExample(example);
+
+            expect(page.getUrl()).toContain(`/examples/${theme}/${example.id}`);
+          });
+        });
       });
     });
   });
--- apps/demo-e2e/src/examples/examples.po.ts
+++ apps/demo-e2e/src/examples/examples.po.ts
@@ -1,7 +1,18 @@
 import { Page } from '../page-base';
 
+export interface Example {
+   id: string;
+   modelId: string;
+   name: string;
+}
+
 export class ExamplesPage extends Page {
   constructor(theme: string) {
     super(`/examples/${theme}`);
+  }
+
+  navigateToExample(example: Example): void {
+    const relativeUrl = example.modelId ? `${example.id}/models/${example.modelId}` : example.id;
+    this.navigateTo(relativeUrl);
   }
 }
