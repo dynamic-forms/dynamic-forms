f2a0af82925aaa2c28e6f169c0fd070e0e6f7f69
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2020-07-18 14:00:51 +0200

    Fixed styles for dynamic form actions in combination with icon buttons

--- apps/demo-e2e/src/examples/examples.e2e-spec.ts
+++ apps/demo-e2e/src/examples/examples.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { ExamplesMenu, ExamplesMenuItem } from 'apps/demo/src/app/layout/header/examples-menu/examples-menu';
+import { by, element } from 'protractor';
 import { Example, ExamplesPage } from './examples.po';
 
 const examplesConfig = require('../../../demo/src/assets/examples-menu.json');
@@ -37,10 +38,29 @@ describe('dynamic-forms-demo examples', () => {
           : `for example "${example.name}" with id "${example.id}"`;
 
         describe(description, () => {
-          it('has url and title', () => {
+          it('has url, title and form', () => {
             page.navigateToExample(example);
 
+            const root = element(by.tagName('dynamic-form'));
+            const wrapper = root.element(by.css('.dynamic-form-wrapper'));
+            const form = wrapper.element(by.css('.dynamic-form'));
+            const formElements = form.all(by.tagName('dynamic-form-element'));
+            const formActions = form.element(by.css('.dynamic-form-actions'));
+            const formControlElements = form.all(by.tagName('dynamic-form-control'));
+            const formActionElements = formActions.all(by.tagName('dynamic-form-element'));
+
             expect(page.getUrl()).toContain(`/examples/${theme}/${example.id}`);
+            expect(wrapper.getAttribute('class')).toContain(theme);
+            expect(form.getTagName()).toBe('form');
+            expect(formElements.count()).toBeGreaterThan(0);
+
+            formControlElements.count().then(count => {
+              if (count > 0) {
+                expect(formActionElements.count()).toBeGreaterThan(0);
+              } else {
+                expect(formActionElements.count()).toBe(0);
+              }
+            });
           });
         });
       });
