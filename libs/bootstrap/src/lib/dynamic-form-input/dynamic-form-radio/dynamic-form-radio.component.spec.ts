749950f0186baeb7fd95f4c4835dfa6afc8f6ba0
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2020-09-26 16:21:32 +0200

    Support of disabled options for dynamic form select and dynamic form radio

--- libs/bootstrap/src/lib/dynamic-form-input/dynamic-form-radio/dynamic-form-radio.component.spec.ts
+++ libs/bootstrap/src/lib/dynamic-form-input/dynamic-form-radio/dynamic-form-radio.component.spec.ts
@@ -37,7 +37,8 @@ describe('BsDynamicFormRadioComponent', () => {
         input: {
           options: [
             { value: 'value1', label: 'label1' },
-            { value: 'value2', label: 'label2' }
+            { value: 'value2', label: 'label2' },
+            { value: 'value3', label: 'label3', disabled: true}
           ]
         }
       }
@@ -63,16 +64,23 @@ describe('BsDynamicFormRadioComponent', () => {
     const inputElements = <HTMLInputElement[]>inputDebugElements.map(elem => elem.nativeElement);
     const labelElements = <HTMLLabelElement[]>labelDebugElements.map(elem => elem.nativeElement);
 
-    expect(inputElements.length).toBe(2);
-    expect(labelElements.length).toBe(2);
+    expect(inputElements.length).toBe(3);
+    expect(labelElements.length).toBe(3);
     expect(inputElements[0].id).toBe(`key-0`);
+    // expect(inputElements[0].name).toBe('key');
     expect(inputElements[0].type).toBe('radio');
     expect(labelElements[0].htmlFor).toBe(`key-0`);
     expect(labelElements[0].innerText).toBe('label1');
     expect(inputElements[1].id).toBe('key-1');
+    // expect(inputElements[1].name).toBe('key');
     expect(inputElements[1].type).toBe('radio');
     expect(labelElements[1].htmlFor).toBe(`key-1`);
     expect(labelElements[1].innerText).toBe('label2');
+    expect(inputElements[2].id).toBe('key-2');
+    // expect(inputElements[2].name).toBe('key');
+    expect(inputElements[2].type).toBe('radio');
+    expect(labelElements[2].htmlFor).toBe(`key-2`);
+    expect(labelElements[2].innerText).toBe('label3');
   });
 
   it('sets dynamic form control to readonly', () => {
@@ -82,11 +90,13 @@ describe('BsDynamicFormRadioComponent', () => {
 
     expect(inputElements[0].readOnly).not.toBe(true);
     expect(inputElements[1].readOnly).not.toBe(true);
+    expect(inputElements[2].readOnly).not.toBe(true);
 
     component.template.readonly = true;
     fixture.detectChanges();
 
     expect(inputElements[0].readOnly).toBe(true);
     expect(inputElements[1].readOnly).toBe(true);
+    expect(inputElements[2].readOnly).toBe(true);
   });
 });
