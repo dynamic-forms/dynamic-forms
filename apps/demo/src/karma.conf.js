12b11108a7964cc0ba65c68b5884dce065a746d8
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2021-01-07 19:57:39 +0100

    Update of karma

--- apps/demo/src/karma.conf.js
+++ apps/demo/src/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     files: [
@@ -18,7 +18,7 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage/@dynamic-forms/demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
