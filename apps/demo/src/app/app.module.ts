fae9d9b9735ffe703167f0584317644109573ef9
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2021-04-25 10:56:38 +0200

    Improvement of app initialization

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppStateModule } from './app-state.module';
 import { AppComponent } from './app.component';
+import { AppService, appInitializer } from './app.service';
 import { DocsModule } from './docs/docs.module';
 import { HomeModule } from './home/home.module';
 import { LayoutModule } from './layout/layout.module';
@@ -28,12 +29,19 @@ import { IconService } from './services/icon.service';
     DocsModule
   ],
   providers: [
+    AppService,
+    IconService,
+    { 
+      provide: APP_INITIALIZER,
+      useFactory: appInitializer, 
+      deps: [AppService], 
+      multi: true
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpRequestInterceptor,
       multi: true
-    },
-    IconService
+    },    
   ],
   bootstrap: [
     AppComponent
---
+++ apps/demo/src/app/app.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { IconService } from './services/icon.service';
+import { ConfigService } from './state/config/config.service';
+import { ExamplesService } from './state/examples/examples.service';
+import { RoutingHandler } from './state/routing/routing.handler';
+
+@Injectable()
+export class AppService {
+  constructor(
+    protected configService: ConfigService,
+    protected examplesService: ExamplesService,
+    protected iconService: IconService,
+    protected routingHandler: RoutingHandler
+  ) {}
+
+  init(): void {
+    this.configService.load();
+    this.examplesService.load();
+    this.iconService.register();
+  }
+}
+
+export function appInitializer(appService: AppService): () => void {
+  return () => appService.init();
+}
