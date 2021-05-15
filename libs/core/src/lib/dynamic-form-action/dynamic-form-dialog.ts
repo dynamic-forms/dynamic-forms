6f03e0b39d83abf076c7304b712fe51cb2a48f6f
Alexander Gebuhr <65446776+AlexanderGebuhr@users.noreply.github.com>
2021-05-15 11:12:21 +0200

    Fixed dynamic form dialog actions

--- libs/core/src/lib/dynamic-form-action/dynamic-form-dialog.ts
+++ libs/core/src/lib/dynamic-form-action/dynamic-form-dialog.ts
@@ -5,6 +5,7 @@ import { DynamicFormBuilder } from '../dynamic-form/dynamic-form.builder';
 import { DynamicFormAction } from './dynamic-form-action';
 
 export class DynamicFormDialog extends DynamicForm {
+  protected _
   protected _action: DynamicFormAction;
 
   constructor(builder: DynamicFormBuilder, action: DynamicFormAction, definition: DynamicFormDefinition, model: any) {
@@ -15,14 +16,14 @@ export class DynamicFormDialog extends DynamicForm {
   get action(): DynamicFormAction { return this._action; }
 
   protected getChildren(): DynamicFormElement[] {
-    return this._builder.createFormElements(this.root, this, this.definition.children);
+    return this._builder.createFormElements(this, this, this.definition.children);
   }
 
   protected getHeaderActions(): DynamicFormAction[] {
-    return this._builder.createFormActions(this.root, this.action, this.definition.headerActions);
+    return this._builder.createFormActions(this.action.root, this.action, this.definition.headerActions);
   }
 
   protected getFooterActions(): DynamicFormAction[] {
-    return this._builder.createFormActions(this.root, this.action, this.definition.footerActions);
+    return this._builder.createFormActions(this.action.root, this.action, this.definition.footerActions);
   }
 }
