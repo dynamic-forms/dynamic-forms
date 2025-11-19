30d7e34edf8c711dc7d7479348e8871b1ff74f29
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2025-11-19 20:59:41 +0100

    Improvements regarding light and dark mode of docs created with compodoc

--- apps/demo/src/app/docs/docs.component.ts
+++ apps/demo/src/app/docs/docs.component.ts
@@ -1,5 +1,10 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, ElementRef, computed, input, viewChild } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Store } from '@ngxs/store';
+import { distinctUntilChanged } from 'rxjs';
+import { ThemeClass } from '../state/preferences/preferences.model';
+import { PreferencesState } from '../state/preferences/preferences.state';
 
 @Component({
   selector: 'app-docs',
@@ -11,6 +16,38 @@ export class DocsComponent {
   readonly sourceUrl = input<string>(undefined);
   readonly scrolling = input<boolean>(undefined);
   readonly trustedSourceUrl = computed(() => this.sanitizer.bypassSecurityTrustResourceUrl(this.sourceUrl()));
+  readonly iframe = viewChild<ElementRef<HTMLIFrameElement>>('iframe');
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(
+    private store: Store,
+    private sanitizer: DomSanitizer,
+  ) {
+    this.store
+      .select(PreferencesState.themeClass)
+      .pipe(takeUntilDestroyed(), distinctUntilChanged())
+      .subscribe(themeClass => this.setThemeClass(themeClass));
+  }
+
+  updateThemeClass(): void {
+    const themeClass = this.store.selectSnapshot(PreferencesState.themeClass);
+    this.setThemeClass(themeClass);
+  }
+
+  private setThemeClass(themeClass: ThemeClass): void {
+    const sourceUrl = this.sourceUrl();
+    if (!sourceUrl?.includes('docs')) {
+      return;
+    }
+
+    const document = this.iframe()?.nativeElement.contentWindow?.document;
+    if (!document) {
+      return;
+    }
+
+    if (themeClass === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
 }
